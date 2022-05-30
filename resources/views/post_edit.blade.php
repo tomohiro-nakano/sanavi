c1cedd77e8fd6a7450382bba59613dd375502797
tomohiro-nakano <nakano.rivermee@gmail.com>
2022-05-30 17:18:28 +0900

    post_edit_update

--- resources/views/post_edit.blade.php
+++ resources/views/post_edit.blade.php
@@ -10,118 +10,162 @@
         <input type='hidden' name='place_id' value="{{ $post->id }}">
         @include('common.errors')
 
-        <div class="form-group col-md-6 col-xs-12">
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <div class="input-group-text"><i class="fa fa-check-square-o" aria-hidden="true"></i>　施 設 名</div>
+        <div>
+            <div class="container">
+                <div class="form-group col-md-6 col-xs-12">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text-2"><i class="fa fa-check-square-o" aria-hidden="true"></i>　施 設 名
+                            </div>
+                        </div>
+                        <input disabled class="form-control bg-light" style="font-size:16px"
+                            value="{{ $place[0]->place_name }}">
+                    </div>
                 </div>
-                <input disabled class="form-control bg-light" style="font-size:16px" value="{{ $place[0]->place_name }}">
             </div>
         </div>
-        <div id="star">
-            <div class="form-group col-md-6 col-xs-12">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="fa fa-star-half-o" aria-hidden="true"></i>　総合評価</div>
+        <div id="star-all">
+            <div class="container">
+                <div class="form-group col-md-6 col-xs-12">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text-2"><i class="fa fa-star-half-o" aria-hidden="true"></i>　総合評価</div>
+                        </div>
+                        <star-all :star-size="25" :increment="0.5" text-class="custom-text"
+                            :rating="3.0" :round-start-rating="false" @rating-selected="setRating"
+                            class="form-control bg-light"></star-all>
+                        <input type="hidden" name="all_score" :value="this.rating" />
                     </div>
-                    <star-rating :star-size="25" :increment="0.5" text-class="custom-text" :rating="3.0"
-                        :round-start-rating="false" @rating-selected="setRating"></star-rating>
-                    <input type="hidden" name="all_score" :value="this.rating" />
                 </div>
             </div>
-            <div class="form-group col-md-6 col-xs-12">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="fa fa-star-half-o" aria-hidden="true"></i>　ととのい度</div>
-                    </div>
-                        <star-rating :star-size="25" :increment="0.5" text-class="custom-text" :rating="3.0"
-                            :round-start-rating="false" @rating-selected="setRating"></star-rating>
+        </div>
+        <div id="star-totonoi">
+            <div class="container">
+                <div class="form-group col-md-6 col-xs-12">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text-2"><i class="fa fa-star-half-o" aria-hidden="true"></i>　ととのい度</div>
+                        </div>
+                        <star-totonoi :star-size="25" :increment="0.5" text-class="custom-text"
+                            :rating="3.0" :round-start-rating="false" @rating-selected="setRating"
+                            class="form-control bg-light"></star-totonoi>
                         <input type="hidden" name="totonoi_score" :value="this.rating" />
+                    </div>
                 </div>
             </div>
-            <div class="form-group col-md-6 col-xs-12">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="fa fa-star-half-o" aria-hidden="true"></i>　温度評価</div>
-                    </div>
-                    <div id="rt">
-                        <star-rating :star-size="25" :increment="0.5" text-class="custom-text" :rating="3.0"
-                            :round-start-rating="false"></star-rating>
+        </div>
+        <div id="star-rt">
+            <div class="container">
+                <div class="form-group col-md-6 col-xs-12">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text-2"><i class="fa fa-star-half-o" aria-hidden="true"></i>　温度評価</div>
+                        </div>
+                        <star-rt :star-size="25" :increment="0.5" text-class="custom-text"
+                            :rating="3.0" :round-start-rating="false" @rating-selected="setRating"
+                            class="form-control bg-light"></star-rt>
                         <input type="hidden" name="rt_score" :value="this.rating" />
                     </div>
                 </div>
             </div>
-            <div class="form-group col-md-6 col-xs-12">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="fa fa-star-half-o" aria-hidden="true"></i>　水風呂評価</div>
-                    </div>
-                    <div>
-                        <star-rating :star-size="25" :increment="0.5" text-class="custom-text" :rating="3.0"
-                            :round-start-rating="false"></star-rating>
-                        <input type="hidden" name="wt_score" :value="this.rating" />
-                    </div>
-                </div>
-            </div>
-            <div class="form-group col-md-6 col-xs-12">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="fa fa-star-half-o" aria-hidden="true"></i>　外気浴（休憩）評価</div>
-                    </div>
-                    <div>
-                        <star-rating :star-size="25" :increment="0.5" text-class="custom-text" :rating="3.0"
-                            :round-start-rating="false"></star-rating>
-                        <input type="hidden" name="rest_score" :value="this.rating" />
-                    </div>
-                </div>
-            </div>
-            <div class="form-group col-md-6 col-xs-12">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="fa fa-star-half-o" aria-hidden="true"></i>　混 雑 度</div>
-                    </div>
-                    <div>
-                        <star-rating :star-size="25" :increment="0.5" text-class="custom-text" :rating="3.0"
-                            :round-start-rating="false"></star-rating>
-                        <input type="hidden" name="cong_score" :value="this.rating" />
+        </div>
+        <div id="star-wt">
+            <div class="container">
+                <div class="form-group col-md-6 col-xs-12">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text-2"><i class="fa fa-star-half-o" aria-hidden="true"></i>　水風呂評価</div>
+                        </div>
+                        <div>
+                            <star-wt :star-size="25" :increment="0.5" text-class="custom-text"
+                                :rating="3.0" :round-start-rating="false" @rating-selected="setRating"
+                                class="form-control bg-light">
+                            </star-wt>
+                            <input type="hidden" name="wt_score" :value="this.rating" />
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="container">
-            <div class="form-group col-md-6 col-xs-12">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="fa fa-calendar-o"></i>　訪 問 日</div>
+        <div id="star-rest">
+            <div class="container">
+                <div class="form-group col-md-6 col-xs-12">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text-2"><i class="fa fa-star-half-o" aria-hidden="true"></i>　外気浴（休憩）評価
+                            </div>
+                        </div>
+                        <div>
+                            <star-rest :star-size="25" :increment="0.5" text-class="custom-text"
+                                :rating="3.0" :round-start-rating="false" @rating-selected="setRating"
+                                class="form-control bg-light">
+                            </star-rest>
+                            <input type="hidden" name="rest_score" :value="this.rating" />
+                        </div>
                     </div>
-                    <input type="date" name="visit_date" class="form-control datetimepicker-input"
-                        value="{{ old('visit_date', isset($post->visit_date) ? $post->visit_date : '') }}" id="datetimepicker" data-toggle="datetimepicker"
-                        data-target="#datetimepicker">
                 </div>
             </div>
         </div>
-        <div class="container">
-            <div class="form-group col-md-6 col-xs-12">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="fa fa-commenting-o" aria-hidden="true"></i>　感　想</div>
+        <div id="star-cong">
+            <div class="container">
+                <div class="form-group col-md-6 col-xs-12">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text-2"><i class="fa fa-star-half-o" aria-hidden="true"></i>　混 雑 度</div>
+                        </div>
+                        <div>
+                            <star-cong :star-size="25" :increment="0.5" text-class="custom-text"
+                                :rating="3.0" :round-start-rating="false" @rating-selected="setRating"
+                                class="form-control bg-light">
+                            </star-cong>
+                            <input type="hidden" name="cong_score" :value="this.rating" />
+                        </div>
                     </div>
-                    <textarea name="comment" cols="50" rows="10">{{ $post->comment }}</textarea>
                 </div>
             </div>
         </div>
-        <div class="form-group col-3">
-            <div class="d-grid gap-2">
-                <a href="{{ url('/place_list/' . $post->place_id . '/detail') }}" class="btn btn-outline-secondary btn-lg">
-                    <i class="fa fa-mail-reply"></i>　キャンセル
-                </a>
+        <div>
+            <div class="container">
+                <div class="form-group col-md-6 col-xs-12">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text-2"><i class="fa fa-calendar-o"></i>　訪 問 日</div>
+                        </div>
+                        <input type="date" name="visit_date" class="form-control datetimepicker-input"
+                            value="{{ old('visit_date', isset($post->visit_date) ? $post->visit_date : '') }}"
+                            id="datetimepicker" data-toggle="datetimepicker" data-target="#datetimepicker">
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="form-group col-9">
-            <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-secondary btn-lg">
-                    <i class="fa fa-chevron-right"></i>　送 信
-                </button>
+        <div>
+            <div class="container">
+                <div class="form-group col-md-6 col-xs-12">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text-2"><i class="fa fa-commenting-o" aria-hidden="true"></i>　感　想</div>
+                        </div>
+                        <textarea name="comment" cols="50" rows="10">{{ $post->comment }}</textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="post-button">
+            <div class="form-group col-md-3 col-xs-12">
+                <div class="d-grid gap-2">
+                    <a href="{{ url('/place_list/' . $post->place_id . '/detail') }}"
+                        class="btn btn-outline-secondary btn-outline btn-lg">
+                        <i class="fa fa-mail-reply"></i>　キャンセル
+                    </a>
+                </div>
+            </div>
+            <div class="form-group col-md-6 col-xs-12">
+                <div class="d-grid gap-2">
+                    <button type="submit" class="btn btn-secondary btn-lg">
+                        <i class="fa fa-chevron-right"></i>　送 信
+                    </button>
+                </div>
             </div>
         </div>
     </form>
