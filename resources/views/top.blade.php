6ac229dc5cee1c3f8120c1beef9715727e543678
tomohiro-nakano <nakano.rivermee@gmail.com>
2022-06-04 14:35:18 +0900

    about_logo_resize

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -132,7 +132,7 @@
         <div class="about" id="about">
             <div class="row centered">
                 <div class="col-lg-8 col-lg-offset-2">
-                    <img src="img/logo/logo_transparent.png" width="auto" height="400px">
+                    <img src="img/logo/logo_transparent.png" width="auto" height="160px">
                     <h3>Sanaviは国内のサウナレビューサービスです</h3>
                     <p class="about-text">サウナ施設の情報投稿・口コミを閲覧できる機能をベースに、「ととのい度・室温・水温・外気浴・混雑度」の５項目＋総合評価の６項目
                         でレビューができます。サウナ施設に行った記録やメモ・備忘録としてもお使いいただけます。感想や情報を共有してコミュニケーションツールとしてご利用いただけます。
